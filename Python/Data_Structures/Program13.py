a63adfc242d6814392a71a244404b93d6017fb53
Jeel-Doshi <jeeld0311@gmail.com>
2022-03-17 11:08:53 +0530

    Optimized code

--- Python/Data_Structures/Program13.py
+++ Python/Data_Structures/Program13.py
@@ -1,18 +1,25 @@
 # Return the array which contains the elements that are greater than from its right side elements
 
+# Method -1
 input_array = [5, 17, 2, 6, 3]
 output = []
 
-for element in range(len(input_array)):
-    for next_elements in range(element+1,len(input_array)):
-        if input_array[element] < input_array[next_elements]:
-            break
-        else:
-            output.append(input_array[element])
-            break
+output.append(input_array[-1])
 
-print(output + [input_array[-1]])
+for element in range(-2,-len(input_array)-1,-1):
+    if input_array[element] > output[0]:
+        output.insert(0,input_array[element])
 
-# [3, 14, 3, 7, 2]
-# [14, 7, 2]
+print(output)
+
+# Method -2
+# for element in range(len(input_array)):
+#     for next_elements in range(element+1,len(input_array)):
+#         if input_array[element] < input_array[next_elements]:
+#             break
+#         else:
+#             output.append(input_array[element])
+#             break
+
+# print(output + [input_array[-1]])
 
